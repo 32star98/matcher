fdc106600d95741f7523544593f6d7e8a15975e1
32star98 <1196362573xin@sina.com>
2017-07-16 10:25:58 +0800

    逻辑已崩溃

--- Matcher.cpp
+++ Matcher.cpp
@@ -1,16 +1,89 @@
 #include "stdafx.h"
 #include "Matcher.h"
 
-Matcher::Matcher(int state, char * sou, int pointer)
+Matcher::Matcher(const char * sou)
 {
+	this->sou = (char*)sou;
+	this->state = 0;
+	this->pointer = 0;
+	this->root = 0;
+	this->father = 0;
+	init();
 }
 
 Matcher::Matcher(int state, char * sou, int pointer, Matcher * root, Matcher * father)
 {
+	this->state = state;
+	this->sou = sou;
+	this->pointer = pointer;
+	this->root = root;
+	this->father = father;
+	init();
 }
 
 Matcher::~Matcher()
 {
 	if (!childs) delete(childs);
 	if (!chars) delete(chars);
+}
+
+void Matcher::init()
+{
+	if (state == 0) {
+		for (;pointer;pointer++) {
+
+		}
+	}
+	else if (state == 1) {
+
+	}
+	/*else if (state == 2) {
+
+	}
+	else if (state == 3) {
+
+	}*/
+	else
+		matcher_error();
+}
+
+void Matcher::match(char * ins)
+{
+}
+
+void Matcher::matcher_ok(Matcher * child, int endpoint)
+{
+}
+
+void Matcher::matcher_error()
+{
+}
+
+int Matcher::getlength(char * ins)
+{
+	int i = 0;
+	while (ins[i] != '\0') i++;
+	return i < 100 ? i : 100;
+}
+
+int Matcher::belongs(char c)
+{
+	const char* S0 = "([{";	//初始化新序列
+	const char* S1 = ".|^\\";
+	const char* E0 = ")]}"; //终止序列
+	const char* E1 = "+*?";
+	const char* Ns = "0123456789";//数字类
+	if (state == 0) {
+	}
+	else if (state == 1) {
+
+	}
+	else if (state == 2) {
+
+	}
+	else if (state == 3) {
+
+	}
+	else
+	return -1;
 }
--- Matcher.h
+++ Matcher.h
@@ -5,12 +5,15 @@ class Matcher
 {
 //公有函数
 public:
-	Matcher(int state, char* sou, int pointer);
+	Matcher(const char* sou);
 	Matcher(int state, char* sou, int pointer, Matcher* root, Matcher* father);
 	~Matcher();
 
 private:
-	int state;//0,默认, 1圆括号， 2方括号， 3花括号
+	int pointer;
+	Matcher* root;
+	Matcher* father;
+	void init();
 
 	//数量控制变量
 	int num_gate = 0;		//掩码0，默认状态， 只匹配一次，1 激活状态
@@ -19,8 +22,8 @@ private:
 	int mode;				//数量匹配模式，默认0表示贪婪法，1表示懒惰法
 
 	//连接管控
-	int child_gate = 1;		//后续连接结构数量，默认1表示1个
-	Matcher* childs = 0;	//后续量的指针
+	int child_gate = 1;		//内部连接结构数量，默认1表示1个
+	Matcher* childs = 0;	//内部量的指针
 
 	//逻辑管控				//非逻辑控制,默认为1（真）
 	int used = 1;
@@ -37,7 +40,20 @@ private:
 	int up = 0;
 
 public:
+
+	void match(char* ins);
+
+	int state;				//0,默认, 1圆括号， 2方括号， 3花括号
 	int ans_static = 1;		//分析状态控制：0结束分析，1正常进行，2待定义（初始化和匹配时都需要使用）
+	char* sou;
 	void drop();			//结构销毁
+	//void matcher_or(Matcher* child, int endpoint);	//申请或操作
+	void matcher_ok(Matcher* child, int endpoint);	//分析终止申请
+	void matcher_error();				//错误申请
+
+private:
+	//辅助函数
+	int getlength(char* ins);	//最大长度100字符
+	int belongs(char c);		//特殊字符初级判断
 };
 
